88bc6fdd092406c01d6dc5b69e0475e7ab5e07ba
Nuno Maduro <enunomaduro@gmail.com>
2018-09-21 10:19:34 +0200

    Adds model related config

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -4,6 +4,48 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Searchable Attributes
+    |--------------------------------------------------------------------------
+    |
+    | Limits the scope of a search to the attributes listed in this setting. Defining
+    | specific attributes as searchable is critical for relevance because it gives
+    | you direct control over what information the search engine will look at.
+    |
+    | Example: ["attribute1", "ordered(attribute4)", "unordered(attribute5)",]
+    |
+    */
+    'searchableAttributes' => {!! $searchableAttributes  !!},
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes For Faceting
+    |--------------------------------------------------------------------------
+    |
+    | Your index comes with no categories. By designating an attribute as a facet, this enables
+    | Algolia to compute a set of possible values that can later be used to create categories
+    | or filters. You can also get a count of records that match those values.
+    |
+    | Example: ['attribute1', 'filterOnly(attribute2)', 'searchable(attribute3)',]
+    |
+    */
+    'attributesForFaceting' => {!! $attributesForFaceting  !!},
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes To Retrieve
+    |--------------------------------------------------------------------------
+    |
+    | You don’t always need to retrieve a full response that includes every attribute
+    | in your index. Sometimes you may only want to receive the most relevant
+    | attributes, or exclude attributes used only for internal purposes.
+    |
+    | Example: ['attribute1', 'attribute2', 'attribute3',]
+    |
+    */
+    'attributesToRetrieve' => {!! $attributesToRetrieve  !!},
+
+    /*
+    |--------------------------------------------------------------------------
     | Custom Ranking
     |--------------------------------------------------------------------------
     |
