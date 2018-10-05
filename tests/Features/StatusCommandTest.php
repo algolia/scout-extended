f3eca3862631a5417628eea7ba24bdc8c734d513
Nuno Maduro <enunomaduro@gmail.com>
2018-10-05 14:09:16 +0200

    Tests status command

--- tests/Features/StatusCommandTest.php
+++ tests/Features/StatusCommandTest.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace Tests\Features;
 
+use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
 
 final class StatusCommandTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(database_path('migrations'));
+        $this->artisan('migrate:fresh', ['--database' => 'testbench'])->run();
+    }
+
     public function testStatus(): void
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        $usersIndex = $this->mockIndex(User::class, $this->defaults());
+
+        $usersIndex->shouldReceive('search')->andReturn([
+            'nbHits' => 0,
+        ]);
+
+        Artisan::call('scout:status', ['model' => User::class]);
     }
 }
