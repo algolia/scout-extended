a111caea0883d97f3db83714a25fbebaa8fa75cd
Lucas Michot <lucas@semalead.com>
2019-01-26 13:06:03 +0100

    Split factories.

--- tests/laravel/database/factories/UserFactory.php
+++ tests/laravel/database/factories/UserFactory.php
@@ -2,28 +2,15 @@
 
 declare(strict_types=1);
 
-use App\Post;
 use App\User;
-use App\Thread;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
-
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        'subject' => $faker->text,
-    ];
-});
-
-$factory->define(Thread::class, function (Faker $faker) {
-    return [
-        'body' => $faker->text,
-    ];
-});
 
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
