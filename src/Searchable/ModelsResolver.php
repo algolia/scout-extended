49659890e105095a0c0afe0fe98d7db0963f9ea1
Mark Beech <mbeech@mark-beech.co.uk>
2019-01-29 21:57:12 +0000

    Allow eager loading relationships on Aggregated models

--- src/Searchable/ModelsResolver.php
+++ src/Searchable/ModelsResolver.php
@@ -54,7 +54,7 @@ final class ModelsResolver
 
             if (in_array(Searchable::class, class_uses_recursive($model), true)) {
                 if (! empty($models = $model->getScoutModelsByIds($builder, $modelKeys))) {
-                    $instances = $instances->merge($models);
+                    $instances = $instances->merge($models->load($searchable->getRelations($modelClass)));
                 }
             } else {
                 $query = in_array(SoftDeletes::class, class_uses_recursive($model),
@@ -67,7 +67,7 @@ final class ModelsResolver
                 $scoutKey = method_exists($model,
                     'getScoutKeyName') ? $model->getScoutKeyName() : $model->getQualifiedKeyName();
                 if ($models = $query->whereIn($scoutKey, $modelKeys)->get()) {
-                    $instances = $instances->merge($models);
+                    $instances = $instances->merge($models->load($searchable->getRelations($modelClass)));
                 }
             }
         }
