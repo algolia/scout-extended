f66d7b7bea672f79963462809d159383164ceed8
Nuno Maduro <enunomaduro@gmail.com>
2019-02-12 13:36:03 +0100

    Tests unsearchable

---
+++ tests/Features/UnsearchableTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+
+final class UnsearchableTest extends TestCase
+{
+    public function testUnsearchable(): void
+    {
+        factory(User::class, 5)->create();
+
+        $usersIndex = $this->mockIndex(User::class);
+        $usersIndex->shouldReceive('deleteBy')->once()->with([
+            'tagFilters' => [
+                ['App\User::1', 'App\User::2','App\User::3','App\User::4','App\User::5',],
+            ],
+        ]);
+
+        User::get()->unsearchable();
+    }
+}
