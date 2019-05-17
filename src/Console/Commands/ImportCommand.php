39e46b8b01c09a1029e8e83b4a17f05cdb423f38
Nathan Brennan <nathan@healthengine.com.au>
2019-05-17 10:01:48 +0800

    WIP: early draft of possible solution

--- src/Console/Commands/ImportCommand.php
+++ src/Console/Commands/ImportCommand.php
@@ -13,8 +13,11 @@ declare(strict_types=1);
 
 namespace Algolia\ScoutExtended\Console\Commands;
 
+use Algolia\AlgoliaSearch\SearchClient;
 use Illuminate\Console\Command;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Laravel\Scout\Events\ModelsImported;
 use Illuminate\Contracts\Events\Dispatcher;
 use Algolia\ScoutExtended\Helpers\SearchableFinder;
@@ -35,16 +38,57 @@ final class ImportCommand extends Command
     /**
      * {@inheritdoc}
      */
-    public function handle(Dispatcher $events, SearchableFinder $searchableFinder): void
+    public function handle(Dispatcher $events, SearchableFinder $searchableFinder, SearchClient $client): void
     {
         foreach ($searchableFinder->fromCommand($this) as $searchable) {
-            $this->call('scout:flush', ['searchable' => $searchable]);
 
             $events->listen(ModelsImported::class, function ($event) use ($searchable) {
                 $this->resultMessage($event->models, $searchable);
             });
 
             $searchable::makeAllSearchable();
+
+            $this->line("Checking for any records that need to be deleted");
+
+            $model = new $searchable();
+            $index = $client->initIndex($model->searchableAs());
+
+
+            $builder = new \Laravel\Scout\Builder($model, '');
+
+            // check for objects that should be deleted.
+            $iterator = $index->browseObjects([
+                'hitsPerPage' => 100
+            ]);
+
+            foreach ($iterator as $hit) {
+                $searchKey = [ObjectIdEncrypter::decryptSearchableKey($hit['objectID'])];
+
+                try {
+                    $searchableObject = $model->getScoutModelsByIds($builder, $searchKey);
+
+                    if ($searchableObject->count() === 1) {
+                        if (! $searchableObject[0]->shouldBeSearchable()) {
+                            Log::error("ImportCommand delete item unsearchable", [
+                                'object' => $searchable,
+                                'id' => $hit['objectID']
+                            ]);
+                            $searchableObject[0]->unsearchable();
+                        }
+                    } else {
+                        Log::error("ImportCommand delete item", [
+                            'object' => $searchable,
+                            'id' => $hit['objectID']
+                        ]);
+                        $index->deleteObject($hit['objectID']);
+                        $this->line("Found item to delete " . $hit['objectID']);
+                    }
+                } catch (QueryException $e) {
+                    // this should only happen when they are type issues like id is int, and the key is alpha.
+                    $index->deleteObject($hit['objectID']);
+                    $this->line("Found item to delete " . $hit['objectID']);
+                }
+            }
 
             $events->forget(ModelsImported::class);
 
